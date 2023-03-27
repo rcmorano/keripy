47ded5ccdefc707f72afb363a1b4ba1ed5e74939
Roberto C. Morano <rcmorano@none.guru>
2023-03-27 10:16:01 +0200

    chore: added environment variables to set config dir and config file dynamically

--- scripts/demo/backer/start_backer.sh
+++ scripts/demo/backer/start_backer.sh
@@ -1,7 +1,10 @@
-rm -r ~/.keri
+#!/bin/bash
 
-kli init --name witroot --nopasscode --config-dir /Users/rodo/Code/LosDemas/keripy/scripts/demo/roots --config-file witroot
+BACKER_CONFIG_FILE=${BACKER_CONFIG_FILE:-witroot-config.json}
+WITROOTS_CONFIG_DIR=${WITROOTS_CONFIG_DIR:-~/.keri/roots}
 
-kli incept --name witroot --alias witroot --config /Users/rodo/Code/LosDemas/keripy/scripts/demo/roots --file witroot_cfg.json
+kli init --name witroot --nopasscode --config-dir ${WITROOTS_CONFIG_DIR} --config-file witroot
+
+kli incept --name witroot --alias witroot --config ${WITROOTS_CONFIG_DIR} --file ${BACKER_CONFIG_FILE}
 
 kli backer start --name witroot --alias witroot -H 5666 -T 5665 --ledger cardano
